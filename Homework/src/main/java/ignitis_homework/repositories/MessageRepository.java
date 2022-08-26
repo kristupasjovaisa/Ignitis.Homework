5b521588185e1ae2abc4aa9f5874e742cdc7f5ec
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-08-27 00:45:05 +0300

    Added tests for message repository

--- Homework/src/main/java/ignitis_homework/repositories/MessageRepository.java
+++ Homework/src/main/java/ignitis_homework/repositories/MessageRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-
-    Optional<Message> findFirstByFirstMessageDesc();
-
-    Optional<Message> findFirstByLastMessageAsc();
+    Optional<Message> findFirstByOrderByCreatedAtDesc();
+    Optional<Message> findFirstByOrderByCreatedAtAsc();
 }
