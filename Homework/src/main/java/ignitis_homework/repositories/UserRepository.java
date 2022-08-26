b526b53d96343eb880982703139b1673dddc3f60
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-08-26 17:14:47 +0300

    Added UserRepository

---
+++ Homework/src/main/java/ignitis_homework/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package ignitis_homework.repositories;
+
+import ignitis_homework.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
