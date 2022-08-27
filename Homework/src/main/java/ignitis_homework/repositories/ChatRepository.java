8d15e9b0155a902b6d7077e8487ddbf931e27c83
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-08-27 14:34:35 +0300

    Added chat repository

---
+++ Homework/src/main/java/ignitis_homework/repositories/ChatRepository.java
@@ -0,0 +1,9 @@
+package ignitis_homework.repositories;
+
+
+import ignitis_homework.entities.Chat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRepository extends JpaRepository<Chat,Long> {
+
+}
