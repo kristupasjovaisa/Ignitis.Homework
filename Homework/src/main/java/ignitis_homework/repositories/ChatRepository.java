ad63cd577df3ec25b2cb3b03c08cb51e2faec5a6
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-08-28 00:08:48 +0300

    Added method to find all chats by user id

--- Homework/src/main/java/ignitis_homework/repositories/ChatRepository.java
+++ Homework/src/main/java/ignitis_homework/repositories/ChatRepository.java
@@ -5,9 +5,12 @@ import ignitis_homework.entities.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface ChatRepository extends JpaRepository<Chat,Long> {
+public interface ChatRepository extends JpaRepository<Chat, Long> {
     @Query("select chat from Chat chat inner join chat.users user where user.id = :userId")
     Optional<Chat> findByUserId(Long userId);
+    @Query("select chat from Chat chat inner join chat.users user where user.id = :userId")
+    List<Chat> findAllByUserId(Long userId);
 }
--- Homework/src/main/java/ignitis_homework/repositories/UserRepository.java
+++ Homework/src/main/java/ignitis_homework/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package ignitis_homework.repositories;
 import ignitis_homework.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
