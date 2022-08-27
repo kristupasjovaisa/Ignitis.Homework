728245a5ebcab478cab9dd10dc2f715d1db8d29a
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-08-27 15:05:03 +0300

    Fixed extends JpaRepositories to CrudRepositories

--- Homework/src/main/java/ignitis_homework/repositories/MessageRepository.java
+++ Homework/src/main/java/ignitis_homework/repositories/MessageRepository.java
@@ -1,11 +1,11 @@
 package ignitis_homework.repositories;
 
 import ignitis_homework.entities.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface MessageRepository extends JpaRepository<Message, Long> {
+public interface MessageRepository extends CrudRepository<Message, Long> {
     Optional<Message> findFirstByOrderByCreatedAtDesc();
     Optional<Message> findFirstByOrderByCreatedAtAsc();
 }
