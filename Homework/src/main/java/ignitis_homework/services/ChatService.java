da7c082edc9e5ccb35f2f4e37991c603816b77b7
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-08-28 00:09:08 +0300

    Added chat service

--- Homework/src/main/java/ignitis_homework/services/ChatService.java
+++ Homework/src/main/java/ignitis_homework/services/ChatService.java
@@ -1,12 +1,42 @@
 package ignitis_homework.services;
 
+import ignitis_homework.dto.AddChatRequest;
+import ignitis_homework.dto.ChatResponse;
+import ignitis_homework.entities.Chat;
+import ignitis_homework.mapper.ChatMapper;
 import ignitis_homework.repositories.ChatRepository;
+import ignitis_homework.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 
 @Service
 @RequiredArgsConstructor
 public class ChatService {
 
     private final ChatRepository chatRepository;
+    private final UserRepository userRepository;
+    private final ChatMapper mapper;
+
+    public Optional<Chat> addChat(AddChatRequest addChat, Long senderId) {
+        var sender = userRepository.findById(senderId);
+        var receiver = userRepository.findById(addChat.getReceiver());
+        if (sender.isPresent() && receiver.isPresent()) {
+            var senderChat = chatRepository.findByUserId(senderId);
+            var receiverChat = chatRepository.findByUserId(receiver.get().getId());
+            if (senderChat.isPresent() && receiverChat.isPresent() && senderChat.get().getId().equals(receiverChat.get().getId())) {
+                return senderChat;
+            }
+            return Optional.of(chatRepository.save(mapper.mapFrom(sender.get(), receiver.get())));
+        }
+        return Optional.empty();
+    }
+
+    public List<ChatResponse> getChats(Long userId) {
+        return chatRepository.findAllByUserId(userId).stream().map(mapper::mapfrom).collect(Collectors.toList());
+    }
 }
