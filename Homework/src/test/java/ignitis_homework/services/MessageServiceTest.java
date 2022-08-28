bb6bee212e10b31a7b4ba472217f7f2269cece61
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-08-28 21:58:59 +0300

    Fixed message service added isChatContainingUser function

--- Homework/src/test/java/ignitis_homework/services/MessageServiceTest.java
+++ Homework/src/test/java/ignitis_homework/services/MessageServiceTest.java
@@ -17,11 +17,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class MessageServiceTest {
