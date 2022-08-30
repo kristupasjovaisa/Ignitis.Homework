477c197e54e168f2619bd472a2d39a2b5a188983
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-08-31 00:03:52 +0300

    Added UserDetailsServiceImpl

--- Homework/src/main/java/ignitis_homework/repositories/AuthorityRepository.java
+++ Homework/src/main/java/ignitis_homework/repositories/AuthorityRepository.java
@@ -1,14 +1,8 @@
 package ignitis_homework.repositories;
 
 import ignitis_homework.entities.Authority;
-import ignitis_homework.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.Optional;
 
 public interface AuthorityRepository extends JpaRepository<Authority,Long> {
 
-    @Query("SELECT u FROM User u JOIN FETCH u.authorities WHERE u.email = :email")
-    Optional<User> findUserByEmail(String email);
 }
