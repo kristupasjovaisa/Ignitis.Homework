3b55b59a796d2f4911d03789979c0bedd95958a4
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-08-30 23:51:55 +0300

    Added UserRoleDto

---
+++ Homework/src/main/java/ignitis_homework/security/dto/UserRoleDto.java
@@ -0,0 +1,56 @@
+package ignitis_homework.security.dto;
+
+import ignitis_homework.entities.User;
+import lombok.Builder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Builder
+public class UserRoleDto implements UserDetails {
+    private User user;
+    @Builder.Default
+    private Set<? extends GrantedAuthority> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getFullName() {
+        return user.getName();
+    }
+}
