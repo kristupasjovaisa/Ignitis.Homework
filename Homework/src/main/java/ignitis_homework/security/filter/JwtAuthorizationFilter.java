0113f748548dcd3b7dfe18e1cecb0a48e70fd622
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-08-31 13:21:13 +0300

    Added configure JwtAuthorizationFilter

---
+++ Homework/src/main/java/ignitis_homework/security/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,35 @@
+package ignitis_homework.security.filter;
+
+import ignitis_homework.security.service.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    private static final String BEARER = "Bearer";
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String authHeaderValue = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authHeaderValue != null && authHeaderValue.startsWith(BEARER)) {
+            Authentication authentication = jwtProvider.parseToken(authHeaderValue.replace(BEARER, ""));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
