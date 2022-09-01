8d6f8c4a01949ce55ac47495d64a56aa61600659
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-09-01 03:11:05 +0300

    Fixed bugs

--- Homework/src/main/java/ignitis_homework/mappers/MessageMapper.java
+++ Homework/src/main/java/ignitis_homework/mappers/MessageMapper.java
@@ -5,17 +5,21 @@ import ignitis_homework.dto.MessageResponse;
 import ignitis_homework.entities.Chat;
 import ignitis_homework.entities.Message;
 import ignitis_homework.entities.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.sql.Timestamp;
 
 @Component
+@RequiredArgsConstructor
 public class MessageMapper {
+    private final UserMapper userMapper;
 
     public MessageResponse mapFrom(Message message) {
         return MessageResponse.builder()
                 .id(message.getId())
                 .createdAt(String.valueOf(message.getCreatedAt()))
+                .sender(userMapper.mapFrom(message.getOwner()))
                 .text(message.getText())
                 .build();
     }
--- Homework/src/test/java/ignitis_homework/mappers/MessageMapperTest.java
+++ Homework/src/test/java/ignitis_homework/mappers/MessageMapperTest.java
@@ -1,35 +1,43 @@
 package ignitis_homework.mappers;
 
 import ignitis_homework.dto.AddMessageRequest;
+import ignitis_homework.dto.UserResponse;
 import ignitis_homework.entities.Chat;
 import ignitis_homework.entities.Message;
 import ignitis_homework.entities.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.sql.Timestamp;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class MessageMapperTest {
-
+    @Mock
+    UserMapper userMapper;
     @Spy
     @InjectMocks
     MessageMapper messageMapper;
 
     @Test
     void mapToMessageResponse() {
+        var user = User.builder().build();
         var message = Message.builder()
                 .id(1l)
                 .createdAt(Timestamp.valueOf("2022-01-01 00:00:00.123456789"))
+                .owner(user)
                 .text("text")
                 .build();
+        when(userMapper.mapFrom(user)).thenReturn(UserResponse.builder().id(1l).build());
         var actual = messageMapper.mapFrom(message);
         assertEquals(1, actual.getId());
+        assertEquals(1, actual.getSender().getId());
         assertEquals("2022-01-01 00:00:00.123456789", actual.getCreatedAt());
         assertEquals("text", actual.getText());
     }
